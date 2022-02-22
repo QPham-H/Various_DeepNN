b8ded5527a2f42c64aeedf3c465f2f1f41578d9e
Quoc H Pham <quocpham.h@gmail.com>
2022-02-22 11:18:03 -0600

    Update CNN Object Detection.py

--- CNN Object Detection.py
+++ CNN Object Detection.py
@@ -1,210 +1,117 @@
-import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-import pathlib
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
-from IPython.display import display
-from object_detection.utils import ops as utils_ops
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
- 
-while "models" in pathlib.Path.cwd().parts:
-    os.chdir('..')
- 
-def load_model(model_name):
-  base_url = 'http://download.tensorflow.org/models/object_detection/'
-  model_file = model_name + '.tar.gz'
-  model_dir = tf.keras.utils.get_file(
-    fname=model_name, 
-    origin=base_url + model_file,
-    untar=True)
- 
-  model_dir = pathlib.Path(model_dir)/"saved_model"
- 
-  model = tf.saved_model.load(str(model_dir))
- 
-  return model
- 
-PATH_TO_LABELS = 'models/research/object_detection/data/mscoco_label_map.pbtxt'
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
- 
-model_name = 'ssd_inception_v2_coco_2017_11_17'
-detection_model = load_model(model_name)
-
-## 
-def run_inference_for_single_image(model, image):
-  image = np.asarray(image)
-  # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-  input_tensor = tf.convert_to_tensor(image)
-  # The model expects a batch of images, so add an axis with `tf.newaxis`.
-  input_tensor = input_tensor[tf.newaxis,...]
-
-  # Run inference
-  model_fn = model.signatures['serving_default']
-  output_dict = model_fn(input_tensor)
-
-  # All outputs are batches tensors.
-  # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-  # We're only interested in the first num_detections.
-  num_detections = int(output_dict.pop('num_detections'))
-  output_dict = {key:value[0, :num_detections].numpy() 
-                 for key,value in output_dict.items()}
-  output_dict['num_detections'] = num_detections
-
-  # detection_classes should be ints.
-  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-   
-  # Handle models with masks:
-  if 'detection_masks' in output_dict:
-    # Reframe the the bbox mask to the image size.
-    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-              output_dict['detection_masks'], output_dict['detection_boxes'],
-               image.shape[0], image.shape[1])      
-    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                       tf.uint8)
-    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-    
-  return output_dict
-
-def show_inference(model, image_path):
-  # the array based representation of the image will be used later in order to prepare the
-  # result image with boxes and labels on it.
-  image_np = np.array(Image.open(image_path))
-
-
-  # Actual detection.
-    
-  output_dict = run_inference_for_single_image(model, image_np)
-  # Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks_reframed', None),
-      use_normalized_coordinates=True,
-      line_thickness=8)
-
-  display(Image.fromarray(image_np))
-  
-PATH_TO_TEST_IMAGES_DIR = pathlib.Path('models/research/object_detection/test_images')
-TEST_IMAGE_PATHS = sorted(list(PATH_TO_TEST_IMAGES_DIR.glob("*.jpg")))
-
-for image_path in TEST_IMAGE_PATHS:
-    print(image_path)
-    show_inference(detection_model, image_path)
-	
-# Real-Time Object detection using Tensorflow
-import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-import pathlib
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
-from PIL import Image
-from IPython.display import display
-from object_detection.utils import ops as utils_ops
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
- 
-while "models" in pathlib.Path.cwd().parts:
-    os.chdir('..')
- 
-def load_model(model_name):
-  base_url = 'http://download.tensorflow.org/models/object_detection/'
-  model_file = model_name + '.tar.gz'
-  model_dir = tf.keras.utils.get_file(
-    fname=model_name, 
-    origin=base_url + model_file,
-    untar=True)
- 
-  model_dir = pathlib.Path(model_dir)/"saved_model"
-  model = tf.saved_model.load(str(model_dir))
-  return model
- 
-PATH_TO_LABELS = 'models/research/object_detection/data/mscoco_label_map.pbtxt'
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
- 
-model_name = 'ssd_inception_v2_coco_2017_11_17'
-detection_model = load_model(model_name)
- 
-def run_inference_for_single_image(model, image):
-  image = np.asarray(image)
-  # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-  input_tensor = tf.convert_to_tensor(image)
-  # The model expects a batch of images, so add an axis with `tf.newaxis`.
-  input_tensor = input_tensor[tf.newaxis,...]
- 
-  # Run inference
-  model_fn = model.signatures['serving_default']
-  output_dict = model_fn(input_tensor)
- 
-  # All outputs are batches tensors.
-  # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-  # We're only interested in the first num_detections.
-  num_detections = int(output_dict.pop('num_detections'))
-  output_dict = {key:value[0, :num_detections].numpy() 
-                 for key,value in output_dict.items()}
-  output_dict['num_detections'] = num_detections
- 
-  # detection_classes should be ints.
-  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-    
-  # Handle models with masks:
-  if 'detection_masks' in output_dict:
-    # Reframe the the bbox mask to the image size.
-    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-              output_dict['detection_masks'], output_dict['detection_boxes'],
-               image.shape[0], image.shape[1])      
-    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                       tf.uint8)
-    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-     
-  return output_dict
-  
-# All we do is to get frames from the webcam feed and convert them to Numpy arrays.
-def show_inference(model, frame):
-  #take the frame from webcam feed and convert that to array
-  image_np = np.array(frame)
-  # Actual detection.
-     
-  output_dict = run_inference_for_single_image(model, image_np)
-  # Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks_reframed', None),
-      use_normalized_coordinates=True,
-      line_thickness=5)
- 
-  return(image_np)
-  
-#Now we open the webcam and start detecting objects
+# Static Images
+import tensorflow_hub as hub
 import cv2
-video_capture = cv2.VideoCapture(0)
-while True:
-    # Capture frame-by-frame
-    re,frame = video_capture.read()
-    Imagenp=show_inference(detection_model, frame)
-    cv2.imshow('object detection', cv2.resize(Imagenp, (800,600)))
+import numpy
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+width = 1028
+height = 1028
+
+#Load image by Opencv2
+img = cv2.imread('image_2.jpg')
+#Resize to respect the input_shape
+inp = cv2.resize(img, (width , height ))
+
+#Convert img to RGB
+rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+
+# COnverting to uint8
+rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+
+#Add dims to rgb_tensor
+rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+
+import tensorflow_hub as hub
+import pandas as pd
+
+# Loading model directly from TensorFlow Hub
+detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
+
+# Loading csv with labels of classes
+labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
+labels = labels['OBJECT (2017 REL.)']
+
+# Creating prediction
+boxes, scores, classes, num_detections = detector(rgb_tensor)
+
+# Processing outputs
+pred_labels = classes.numpy().astype('int')[0] 
+pred_labels = [labels[i] for i in pred_labels]
+pred_boxes = boxes.numpy()[0].astype('int')
+pred_scores = scores.numpy()[0]
+
+# Putting the boxes and labels on the image
+for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+    if score < 0.5:
+        continue
+
+    score_txt = f'{100 * round(score)}%'
+    img_boxes = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),2)      
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img_boxes, label,(xmin, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
+    cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
+	
+# Live Webcam Video
+import tensorflow_hub as hub
+import cv2
+import numpy
+import tensorflow as tf
+import pandas as pd
+
+# Carregar modelos
+detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
+labels = pd.read_csv('labels.csv',sep=';',index_col='ID')
+labels = labels['OBJECT (2017 REL.)']
+
+cap = cv2.VideoCapture(0)
+
+width = 512
+height = 512
+
+while(True):
+    #Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    #Resize to respect the input_shape
+    inp = cv2.resize(frame, (width , height ))
+
+    #Convert img to RGB
+    rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+
+    #Is optional but i recommend (float convertion and convert img to tensor image)
+    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+
+    #Add dims to rgb_tensor
+    rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+    
+    boxes, scores, classes, num_detections = detector(rgb_tensor)
+    
+    pred_labels = classes.numpy().astype('int')[0]
+    
+    pred_labels = [labels[i] for i in pred_labels]
+    pred_boxes = boxes.numpy()[0].astype('int')
+    pred_scores = scores.numpy()[0]
+   
+   #loop throughout the detections and place a box around it  
+    for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+        if score < 0.5:
+            continue
+            
+        score_txt = f'{100 * round(score,0)}'
+        img_boxes = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),1)      
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
+
+
+
+    #Display the resulting frame
+    cv2.imshow('black and white',img_boxes)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-video_capture.release()
+
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
-"""
