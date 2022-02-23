29006ce897ff0a8a14803ef5f0f8d928f5864cd9
Quoc H Pham <quocpham.h@gmail.com>
2022-02-22 19:37:41 -0600

    Update CNN Object Detection.py

--- CNN Object Detection.py
+++ CNN Object Detection.py
@@ -1,50 +1,47 @@
-# Static Images
-import tensorflow_hub as hub
-import cv2
-import numpy
+import numpy as np
 import pandas as pd
 import tensorflow as tf
+
+import cv2
+import tensorflow_hub
 import matplotlib.pyplot as plt
 
-width = 1028
-height = 1028
+# Constants
+img_wd = 256
+img_ht = 256
+file = 'sample.jpg'
+score_threshold = 0.3
 
-#Load image by Opencv2
-img = cv2.imread('image_2.jpg')
-#Resize to respect the input_shape
-inp = cv2.resize(img, (width , height ))
 
-#Convert img to RGB
-rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+# Load image
+img = cv2.imread(file)
+img = cv2.resize(img, (img_wd,img_ht))
 
-# COnverting to uint8
-rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+# If img is BGR, convert to RGB
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-#Add dims to rgb_tensor
-rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+# The input tensor is a tf.uint8 tensor with shape [None, height, width, 3] with values in [0, 255]
+img_tensor = tf.convert_to_tensor(img) #,dtype=tf.uint8)
+img_tensor = tf.expand_dims(img_tensor, axis = 0)
 
-import tensorflow_hub as hub
-import pandas as pd
-
-# Loading model directly from TensorFlow Hub
-detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
-
-# Loading csv with labels of classes
-labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
-labels = labels['OBJECT (2017 REL.)']
+# Load object detection model using EfficientDet Lite from Tensorflow Hub
+model_link = "https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1"
+detector = hub.load(model_link)
 
 # Creating prediction
 boxes, scores, classes, num_detections = detector(rgb_tensor)
 
-# Processing outputs
-pred_labels = classes.numpy().astype('int')[0] 
-pred_labels = [labels[i] for i in pred_labels]
-pred_boxes = boxes.numpy()[0].astype('int')
-pred_scores = scores.numpy()[0]
+##labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
+##labels = labels['OBJECT (2017 REL.)']
+##
+##pred_labels = classes.numpy().astype('int')[0] 
+##pred_labels = [labels[i] for i in pred_labels]
+##pred_boxes = boxes.numpy()[0].astype('int')
+##pred_scores = scores.numpy()[0]
 
-# Putting the boxes and labels on the image
+
 for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
-    if score < 0.5:
+    if score < score_threshold:
         continue
 
     score_txt = f'{100 * round(score)}%'
@@ -52,7 +49,7 @@ for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_lab
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(img_boxes, label,(xmin, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
     cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
-	
+
 # Live Webcam Video
 import tensorflow_hub as hub
 import cv2
